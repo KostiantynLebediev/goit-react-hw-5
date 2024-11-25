fb275cc570d35ab00a07f19df4f4c206432c2ec9
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2024-11-25 20:42:07 +0000

    +

--- src/components/MoiveList/MovieList.jsx
+++ src/components/MoiveList/MovieList.jsx
@@ -1,9 +1,10 @@
 import css from "./MovieList.module.css";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useSearchParams, useLocation } from "react-router-dom";
 
 export default function MovieList({ movies, from }) {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
+  const location = useLocation();
   const defaultImg =
     "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
@@ -16,6 +17,7 @@ export default function MovieList({ movies, from }) {
             state={{
               from,
               search: query,
+              previousLocation: location,
             }}
             className={css.movieLink}
           >
